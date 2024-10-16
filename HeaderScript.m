bc465407a794f5a032b6cfffa2d35749c2c780b4
Crivcianschi_Alexei <crivcianschialexei@gmail.com>
2024-10-16 10:07:34 +0300

    Update HeaderScript.m

--- HeaderScript.m
+++ HeaderScript.m
@@ -1,20 +1,19 @@
 %========================================
-% Cretu Sabina & Crivcianschi Alexei
+%      Crivcianschi Alexei
 % This header scvript includes all 
 % input variables and values
 %========================================
-
 
 m = 3;
 Un = 415; % [V]
 f = 50; % [Hz]
 connection = 'Y'; % Y connection
-ProtectClass = 'F'; % max. 120*C protected
+ProtectionClass = 'F'; % max. 120*C protected
 ProtectionGroup = "IP22";
 Serviciul = "S1";
 
 % personalized data:
-Pn = 132; % [kW]
-nN = 2978; % [rotations/min]
+Pn = 132000; % [W]
+n = 2978; % [rotations/min]
 
 % Electrical Machine is working in normal conditions
