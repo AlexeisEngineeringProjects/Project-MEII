24598ad7604b46e7a4c9997ceb7ec4bbaf2a6af6
Crivcianschi_Alexei <crivcianschialexei@gmail.com>
2025-03-24 20:35:57 +0200

    Add files via upload

---
+++ src/main.m
@@ -0,0 +1,9 @@
+% run with this command from windows terminal
+run(fullfile(pwd, '..', 'lib', 'Init.m'));
+
+% run with this command from Matlab
+% run(fullfile(pwd, 'lib', 'Init.m'));
+
+run("T1_Dimensions.m");
+
+run("T2_Stator.m");
