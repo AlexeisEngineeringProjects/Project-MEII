54d40f6d4a99fc23991a443f7fcdffe0356a7128
Crivcianschi_Alexei <crivcianschialexei@gmail.com>
2025-03-25 09:27:29 +0200

    Update ME2_Solver.bat

--- ME2_Solver.bat
+++ ME2_Solver.bat
@@ -1,7 +1,7 @@
 @echo off
 color 2
 
-title ME2 Solver
+title ME2_Solver Terminal
 
 echo ===============================================================
 echo                    ## ME2_Solver Utility ##
@@ -21,17 +21,23 @@ set /p answer=Do you want to continue? [Y/N]:
 
 if /i "%answer%"=="Y" goto execution
 
-if /i "%answer%"=="N" goto inputverifi
-
-
-:inputverifi
-echo Insert only "Y" if you an to copntinue or "N" if not
-goto ask
-
+if /i "%answer%"=="N" goto end
+    
 :execution
 matlab.exe -nosplash -nodesktop -r "cd('%mypath%'); HomeDirectory='%mypath%'; run('src\main.m'); exit;"
 echo.
 echo Launching the Matlab Terminal ...
 pause > nul
 
+echo.
+set /p answer=Do you want to repeat script execution? [Y/N]: 
+
+if /i "%answer%"=="Y" goto execution
+
+if /i "%answer%"=="N" goto end
+
 :end
+
+echo.   
+echo press any key to exit
+pause > nul 
