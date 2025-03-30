f9bf41209ade913477a8b404d1172e32fff6d707
Crivcianschi_Alexei <crivcianschialexei@gmail.com>
2025-03-30 13:10:22 +0300

    Update T5Parameters_UserFnc.m

--- lib/T5Parameters_UserFnc.m
+++ lib/T5Parameters_UserFnc.m
@@ -1,4 +1,4 @@
-function [A, x, kj] = T5Parameters_UserFnc()
+function [A, x, kj] = T5Parameters_UserFnc(p)
     % Prompts for each parameter
     promt = ["insert value from table for A:"...
              "insert Δ used for h_{isc} = h_{d2} + Δ; Δ = (5 ... 10) [mm]"...
@@ -8,6 +8,25 @@ function [A, x, kj] = T5Parameters_UserFnc()
     % Default values for the input dialog
     default_values = {'4', '7', '1.38'};
 
+%   ┌────────────┬────────┬────────┬────────┬────────┐
+%   │     p      │   1    │   2    │   3    │   4    │
+%   ├────────────┼────────┼────────┼────────┼────────┤
+%   │   A [cm]   │  -4.0  │  -3.5  │   0.0  │  +2.5  │
+%   └────────────┴────────┴────────┴────────┴────────┘
+ 
+    switch p
+        case 1
+            default_values = {'4', '7', '1.38'};
+        case 2
+            default_values = {'3.5', '7', '1.38'};
+        case 3
+            default_values = {'0', '7', '1.38'};
+        case 4
+            default_values = {'2.5', '7', '1.38'};
+        otherwise
+            disp("[ ERROR ] p out of range! Check initial values.")
+            return;
+    end
    
     % Input dialog options (if you want to customize options)
     opts = struct('WindowStyle', 'normal', 'Interpreter', 'tex');
@@ -20,6 +39,5 @@ function [A, x, kj] = T5Parameters_UserFnc()
     A = str2double(answer{1});
     x = str2double(answer{2});
     kj = str2double(answer{3});
-
 
 end
