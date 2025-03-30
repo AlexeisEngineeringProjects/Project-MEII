87cd7955389cb0351f56ba94652b3ed7b1e26d25
Crivcianschi_Alexei <crivcianschialexei@gmail.com>
2025-03-30 13:08:19 +0300

    Update T1Parameters_UserFnc.m

--- lib/T1Parameters_UserFnc.m
+++ lib/T1Parameters_UserFnc.m
@@ -19,27 +19,32 @@ function [kw, ksd, lambda, kD] = T1Parameters_UserFnc(p)
     % │   kD    │  1.65-1.69 │  1.46-1.49 │  1.37-1.4  │  1.27-1.3  │
     % └─────────┴────────────┴────────────┴────────────┴────────────┘
 
+    default_values = {'0.92', '1.28', '0.37', '1.66'};
+
     % Modify prompts based on the value of p
     switch p
         case 1
             promt(3) = promt(3) + "0.35 ... 1.25";
             promt(4) = promt(4) + "1.65 ... 1.69";
+            default_values = {'0.92', '1.28', '0.37', '1.66'};
         case 2
             promt(3) = promt(3) + "0.5 ... 1.75";
             promt(4) = promt(4) + "1.46 ... 1.49";
+            default_values = {'0.92', '1.28', '1.0', '1.47'};
         case 3
             promt(3) = promt(3) + "0.65 ... 1.90";
             promt(4) = promt(4) + "1.37 ... 1.40";
+            default_values = {'0.92', '1.28', '0.75', '1.385'};
         case 4
             promt(3) = promt(3) + "0.75 ... 2.2";
             promt(4) = promt(4) + "1.27 ... 1.30";
+            default_values = {'0.92', '1.28', '1.5', '1.285'};
         otherwise
             disp("[ ERROR ] p out of range! Check initial values.")
             return;
     end
     
     % Default values for the input dialog
-    default_values = {'0.92', '1.28', '0.37', '1.66'};
     
     % Input dialog options (if you want to customize options)
     opts = struct('WindowStyle', 'normal', 'Interpreter', 'tex');
