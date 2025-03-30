60a5aaa8fb4847f64588d17b8115d5faa3ddacbf
Crivcianschi_Alexei <crivcianschialexei@gmail.com>
2025-03-30 13:09:20 +0300

    Update T2Parameters_UserFnc.m

--- lib/T2Parameters_UserFnc.m
+++ lib/T2Parameters_UserFnc.m
@@ -10,8 +10,8 @@ function [q1, a1, J1, iz, kw1, Bd1, ku, bistm1, histm1, hpana, giz, nf] = T2Para
              "insert  b_{istm1} , b_{istm1} >= d_{ci} + 1.5 [mm] "...
              "insert  h_{istm1}  from 1 ... 2 [mm]"...
              "insert  h_{pana}  from 2 ... 4 [mm]"...
-              "insert  g_{iz} , g_{iz} < 0.44 [mm]"...
-              "insert value for n_{f} from 1 ... 20 (integer)"
+             "insert  g_{iz} , g_{iz} < 0.44 [mm]"...
+             "insert value for n_{f} from 1 ... 20 (integer)"
              ];
 
     % Default values for the input dialog
@@ -30,18 +30,26 @@ function [q1, a1, J1, iz, kw1, Bd1, ku, bistm1, histm1, hpana, giz, nf] = T2Para
             promt(1) = promt(1) + "(4 ... 8)";
             promt(2) = promt(2) + "2";
             promt(3) = promt(3) + " from (5.5 ... 6) [A/mm^{2}]";
+
+            default_values = {'8', '2', '5.8', '0.04', '0.922', '1.7', '0.75', '3.25', '1.8', '3', '0.4', '10'};
         case 2
             promt(1) = promt(1) + "(4 ... 6)";
             promt(2) = promt(2) + "4";
             promt(3) = promt(3) + "from (5 ... 5.5) [A/mm^{2}]";
+            
+            default_values = {'5', '4', '5.3', '0.04', '0.922', '1.7', '0.75', '3.25', '1.8', '3', '0.4', '10'};
         case 3
             promt(1) = promt(1) + "(3 ... 5)";
             promt(2) = promt(2) + "6";
             promt(3) = promt(3) + "from (4.5 ... 5) [A/mm^{2}]";
+
+            default_values = {'4', '3', '4.8', '0.04', '0.922', '1.7', '0.75', '3.25', '1.8', '3', '0.4', '10'};
         case 4
-            promt(1) = promt(1) + + "(2 ... 4)";
+            promt(1) = promt(1) + "(2 ... 4)";
             promt(2) = promt(2) + "8";
             promt(3) = promt(3) + "from (4 ... 5.5) [A/mm^{2}]";
+
+            default_values = {'3', '4', '4.75', '0.04', '0.922', '1.7', '0.75', '3.25', '1.8', '3', '0.4', '10'};
         otherwise
             disp("[ ERROR ] p out of range! Check initial values.")
             return;
